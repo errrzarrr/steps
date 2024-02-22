1c3247719b3ebba8b92d3c1138d7b3c6bd551b61
Roberto Miñagorri <errrzarrr@gmail.com>
2024-02-22 13:14:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
+
+export default function App() {
+	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
+
+	function handlePrevious() {
+		if (step >= 2) setStep((current) => current - 1);
+	}
+
+	function handleNext() {
+		if (step <= 2) {
+			setStep((current) => current + 1);
+		}
+	}
+
+	return (
+		<>
+			<button className="close" onClick={() => setIsOpen((is) => !is)}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? "active" : ""}>1</div>
+						<div className={step >= 2 ? "active" : ""}>2</div>
+						<div className={step >= 3 ? "active" : ""}>3</div>
+					</div>
+
+					<StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+					<div className="buttons">
+						<Button
+							bgColor="#7950f2"
+							textColor="#FFF"
+							onClick={handlePrevious}
+						>
+							<span>👈🏻</span>Previous
+						</Button>
+						<Button
+							bgColor="#7950f2"
+							textColor="#FFF"
+							onClick={handleNext}
+						>
+							Next<span>👉🏻</span>
+						</Button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
-export default App;
+function StepMessage({ step, children }) {
+	return (
+		<div className="message">
+			<h3>Step {step}</h3>
+			{children}
+		</div>
+	);
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+	console.info(children);
+	console.info(typeof children);
+	return (
+		<button
+			style={{
+				backgroundColor: bgColor,
+				color: textColor,
+			}}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+}
